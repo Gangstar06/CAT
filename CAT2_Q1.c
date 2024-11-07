7bee08a85f93673f9705c21fa2d29ac329839f52
Gangstar06 <michaelmasai29@gmail.com>
2024-11-07 23:29:38 +0300

    Update CAT2_Q1.c

--- CAT2_Q1.c
+++ CAT2_Q1.c
@@ -3,15 +3,25 @@
 int main() {
 	int i,j;
     
-    int scores[2][2] = {{62,92},{35,70}};
+    int scores1[2][2] = {{62,92},
+	{84,72}};
+int scores2[2][2]={{35,70},
+	{59,67}};
         
-    printf("scores\n"); 
+    printf("scores1\n"); 
     for(i = 0; i < 2; i++){
         for (j = 0; j < 2; j++){
-        	printf("%d ",scores[i][j]);
+        	printf("%d ",scores1[i][j]);
 		}
 	    printf("\n");	
 	}
+
+	printf("scores2\n"),
+		for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			printf("%d ",scores2[i][j]);
+		}
+		printf("\n");
            
 
     return 0;
